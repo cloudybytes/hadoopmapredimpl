cd3d1474039a157466f66a636dd14adefd5bc6f5
Kalash Shah <kalashshah11@gmail.com>
2021-10-01 16:41:39 +0530

    Final Abstract Table Implementation

--- src/main/java/com/cloudybytes/hadoopmapredimpl/HadoopmapredimplApplication.java
+++ src/main/java/com/cloudybytes/hadoopmapredimpl/HadoopmapredimplApplication.java
@@ -1,13 +1,20 @@
 package com.cloudybytes.hadoopmapredimpl;
 
+import com.cloudybytes.QueryGetter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HadoopmapredimplApplication {
+public class HadoopmapredimplApplication  implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HadoopmapredimplApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args){
+		QueryGetter qg = new QueryGetter();
+		System.out.println(qg.getParsedQuery("SELECT * FROM USERS INNER JOIN RATING on users.userid = rating.userid where age = 5 GROUP BY AGE HAVING AVG(RATING) >= 3"));
+	}
 }
--- src/main/java/utils/tables/Table.java
+++ src/main/java/utils/tables/Table.java
@@ -1,11 +1,45 @@
 package utils.tables;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import java.util.ArrayList;
 
-public interface Table {
-    Object getColumnValue(String columnName);
-    String groupByString(String[] columns);
-    Boolean checkColumnValue(String columnName, String value);
-    Object getAggregate(String operation, String column, ArrayList<Table> arr);
-    Boolean compareAggregate(String column, String operation, String comparisonOperator, String value, ArrayList<Table> arr);
+
+public class Table {
+    ArrayList<Pair<String,String>> row;
+    Table(String[] vals,ArrayList<String> keys){
+        row = new ArrayList<>();
+        int i = 0;
+        for (String key:keys) {
+            row.add(Pair.of(key,vals[i]));
+            i++;
+        }
+    }
+    Object getColumnValue(String columnName) {
+        for (Pair<String,String> x: row) {
+            if(x.getKey().equalsIgnoreCase(columnName))
+                return x.getValue();
+        }
+        return null;
+    }
+
+    String groupByString(String[] columns) {
+        return null;
+    }
+
+    Boolean checkColumnValue(String columnName, String value) {
+        for (Pair<String,String> x: row) {
+            if(x.getKey().equalsIgnoreCase(columnName) && x.getValue().equalsIgnoreCase(value))
+                return true;
+        }
+        return false;
+    }
+
+    Object getAggregate(String operation, String column, ArrayList<Table> arr) {
+        return null;
+    }
+
+    Boolean compareAggregate(String column, String operation, String comparisonOperator, String value, ArrayList<Table> arr) {
+        return null;
+    }
 }
