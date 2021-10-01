24ce9b4ae1eda8b24e6cc3ce0bd1f6dd0a06f8d6
Kalash Shah <kalashshah11@gmail.com>
2021-10-02 03:00:58 +0530

    Join Completed. Inner Outer and Natural Join implemented

---
+++ src/main/java/com/cloudybytes/hadoopmapredimpl/InputDriverJoin.java
@@ -0,0 +1,60 @@
+package com.cloudybytes.hadoopmapredimpl;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+public class InputDriverJoin  extends Configured implements Tool
+{
+    @Override
+    public int run(String[] args) throws Exception
+    {
+        int status = -1;
+        Configuration configuration = getConf();
+        configuration.set("Separator.File1", ",");
+        configuration.set("Separator.File2", ",");
+        configuration.set("Name.File1", "users");
+        configuration.set("Name.File2", "zipcodes");
+        configuration.set("Jointype","leftouter");
+        configuration.set("Separator.Common", ";");
+        Job job = new Job(configuration, "Multiple Input Example");
+        MultipleInputs.addInputPath(job,new Path("users.csv"), TextInputFormat.class, InputMapper1Join.class);
+        MultipleInputs.addInputPath(job, new Path("zipcodes.csv"), TextInputFormat.class, InputMapper2Join.class);
+        job.setJarByClass(InputDriverJoin.class);
+        job.setReducerClass(InputReducerJoin.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileOutputFormat.setOutputPath(job, new Path("output"));
+        job.waitForCompletion(true);
+        status = job.isSuccessful()? 0:-1;
+        return status;
+    }
+    public static void main(String [] args)
+    {
+        int result;
+        try{
+            result= ToolRunner.run(new Configuration(), new InputDriverJoin(), args);
+            if(0 == result)
+            {
+                System.out.println("Job completed Successfully...");
+            }
+            else
+            {
+                System.out.println("Job Failed...");
+            }
+        }
+        catch(Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+}
+
--- src/main/java/utils/tables/Table.java
+++ src/main/java/utils/tables/Table.java
@@ -7,13 +7,70 @@ import java.util.ArrayList;
 
 public class Table {
     ArrayList<Pair<String,String>> row;
-    Table(String[] vals,ArrayList<String> keys){
+    public Table(String[] vals,ArrayList<String> keys){
         row = new ArrayList<>();
         int i = 0;
         for (String key:keys) {
             row.add(Pair.of(key,vals[i]));
             i++;
         }
+    }
+    public static ArrayList<String> getKeys(String tableName){
+        ArrayList<String> a = new ArrayList<String>();
+        if(tableName.equalsIgnoreCase("rating")){
+            a.add("userid");
+            a.add("movieid");
+            a.add("rating");
+            a.add("timestamp");
+        }
+        else if(tableName.equalsIgnoreCase("movies")){
+            a.add("movieid");
+            a.add("title");
+            a.add("releasedate");
+            a.add("unknown");
+            a.add("action");
+            a.add("adventure");
+            a.add("animation");
+            a.add("children");
+            a.add("comedy");
+            a.add("crime");
+            a.add("documentary");
+            a.add("drama");
+            a.add("fantasy");
+            a.add("film_noir");
+            a.add("horror");
+            a.add("musical");
+            a.add("mystery");
+            a.add("romance");
+            a.add("sci_fi");
+            a.add("thriller");
+            a.add("war");
+            a.add("western");
+        }
+        else if(tableName.equalsIgnoreCase("zipcodes")){
+            a.add("zipcode");
+            a.add("zipcodetype");
+            a.add("city");
+            a.add("state");
+        }
+        else if(tableName.equalsIgnoreCase("users")){
+            a.add("userid");
+            a.add("age");
+            a.add("gender");
+            a.add("occupation");
+            a.add("zipcode");
+        }
+        return a;
+    }
+    public static int getIdx(String filename,String columnname){
+        int i = 0;
+        for(String x : Table.getKeys(filename)){
+            if(x.equalsIgnoreCase(columnname)){
+                return i;
+            }
+            i++;
+        }
+        return -1;
     }
     Object getColumnValue(String columnName) {
         for (Pair<String,String> x: row) {
