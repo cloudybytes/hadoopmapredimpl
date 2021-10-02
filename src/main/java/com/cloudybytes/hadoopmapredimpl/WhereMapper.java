0f20c67eef90108417fa7b297e3202f302733d44
Nalin Deepak <nalin.deepak16@gmail.com>
2021-10-02 19:55:00 +0530

    Add MapReduce for where

--- src/main/java/com/cloudybytes/hadoopmapredimpl/WhereMapper.java
+++ src/main/java/com/cloudybytes/hadoopmapredimpl/WhereMapper.java
@@ -4,29 +4,59 @@
  import org.apache.hadoop.io.LongWritable;
  import org.apache.hadoop.io.Text;
  import org.apache.hadoop.mapreduce.Mapper;
+ import org.json.JSONArray;
+ import org.json.JSONObject;
  import utils.tables.Table;
+
+ import java.io.IOException;
 
  public class WhereMapper extends Mapper<LongWritable, Text, Text, Text> {
      private static String commonSeparator;
+     private static JSONObject queryJSON;
+     private static String filename;
      public void setup(Context context){
          Configuration configuration = context.getConfiguration();
          commonSeparator = configuration.get("Separator.Common");
+         filename = configuration.get("Name.File");
      }
 
-     public void map(LongWritable key, Text value, Context context){
+     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
          String[] values = value.toString().split(commonSeparator);
          StringBuilder stringBuilder = new StringBuilder();
 
+         JSONArray whereJson = queryJSON.getJSONArray("where");
 
-//         int remidx = 0;
-//         for(int index=0; index<values.length; index++)
-//         {
-//             if(index != remidx)
-//                 stringBuilder.append(values[index]+commonSeparator);
-//         }
-//         if(values[remidx] != null && !"NULL".equalsIgnoreCase(values[remidx]))
-//         {
-//             context.write(new Text(values[remidx]), new Text(commonSeparator+stringBuilder.toString()));
-//         }
+         Table row = new Table(values, Table.getKeys(filename));
+         Object columnValue = row.getColumnValue((String)whereJson.get(0));
+         String compareOperator = (String) whereJson.get(1);
+         Object compareValue = whereJson.get(2);
+
+         if(compareOperator.equalsIgnoreCase("=")){
+            if(columnValue != compareValue)
+                return;
+         }
+         else if(compareOperator.equalsIgnoreCase(">")){
+            //if(columnValue <= compareValue)
+                return;
+         }
+         else if(compareOperator.equalsIgnoreCase("<")){
+             //if(columnValue >= compareValue)
+                 return;
+         }
+         else if(compareOperator.equalsIgnoreCase(">=")){
+             //if(columnValue < compareValue)
+                 return;
+         }
+         else if(compareOperator.equalsIgnoreCase("<=")){
+             //if(columnValue > compareValue)
+                 return;
+         }
+         else if(compareOperator.equalsIgnoreCase("<>")){
+             if(columnValue == compareValue)
+                 return;
+         }
+
+         Text groupByColumn = new Text(queryJSON.get("group_by_column").toString());
+         context.write(groupByColumn, value);
      }
  }
---
+++ src/main/java/com/cloudybytes/hadoopmapredimpl/WhereReducer.java
@@ -0,0 +1,19 @@
+package com.cloudybytes.hadoopmapredimpl;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.json.JSONObject;
+import org.apache.hadoop.conf.Configuration;
+
+public class WhereReducer extends Reducer<Text, Text, Text, Text> {
+    private static JSONObject queryJSON;
+
+    public void setup(Context context){
+        Configuration configuration = context.getConfiguration();
+        queryJSON = new JSONObject(configuration.get("queryJSONString"));
+    }
+
+    public void reduce(Text key, Iterable<Text> values, Context context){
+        
+    }
+}
